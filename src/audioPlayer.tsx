5d0093dcac7b0c66a4b9e8b140ade1f8e48aa362
jessestratford92 <jessestratford6366@gmail.com>
2023-03-04 10:58:06 +1300

    add smartphone css styling

--- src/audioPlayer.tsx
+++ src/audioPlayer.tsx
@@ -14,24 +14,26 @@ function handlePause() {
 
 const AudioPlayer: React.FC = () => {
   return (
-    <div className="main-audio-player">
-        <div className="album-artwork">
-            <ParticleField particleCount={1000} particleSize={1} color={'white'}/>
-        </div>
-          <div className="song-info">
-            <h1 className="song-title">Nils Frahm</h1>
-            <h1 className="artist-name">Says</h1>
+    <div className="smartphone">
+      <div className="main-audio-player">
+          <div className="album-artwork">
+              <ParticleField particleCount={1000} particleSize={1} color={'white'}/>
           </div>
-      <ReactAudioPlayer
-        src={Music}
-        autoPlay
-        controls
-        onPlay={handlePlay}
-        onPause={handlePause}
-        style={{ width: '100%', height: '50px' }}
-        onEnded={() => console.log('The song has ended')}
-      />
-      </div>
+            <div className="song-info">
+              <h1 className="song-title">Nils Frahm</h1>
+              <h1 className="artist-name">Says</h1>
+            </div>
+        <ReactAudioPlayer
+          src={Music}
+          autoPlay
+          controls
+          onPlay={handlePlay}
+          onPause={handlePause}
+          style={{ width: '100%', height: '50px' }}
+          onEnded={() => console.log('The song has ended')}
+        />
+        </div>
+    </div>
   )}
 
   export default AudioPlayer

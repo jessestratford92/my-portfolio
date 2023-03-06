e8fc44fd5189610f0aa6586c536f38df041b0df4
jessestratford92 <jessestratford6366@gmail.com>
2023-03-06 15:54:07 +1300

    add header and waves animation, update types

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -2,10 +2,24 @@ import React, { useEffect, useRef } from "react";
 import AudioPlayer from "./audioPlayer";
 import "./styles/index.css";
 
+const wave_1 = require("./assets/SVG/wave-1.svg");
+const wave_2 = require("./assets/SVG/wave-2.svg");
+const wave_3 = require("./assets/SVG/wave-3.svg");
+const wave_4 = require("./assets/SVG/wave-4.svg");
+const wave_5 = require("./assets/SVG/wave-5.svg");
+
 const Portfolio: React.FC = () => {
   return (
     <div>
-      <h1>ADAGIO</h1>
+      <header>
+        <img src={wave_1.default} />
+        <img src={wave_2.default} />
+        <img src={wave_3.default} />
+        <img src={wave_4.default} />
+        <img src={wave_5.default} id="shape" />
+        <h1>JESSE STRATFORD</h1>
+        <h2>Creative Software Engineer</h2>
+      </header>
       <AudioPlayer />
     </div>
   );
